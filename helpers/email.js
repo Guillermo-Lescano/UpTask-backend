f3ce6a7a3cdef6ca8a6cf043309bb2fdf25caba5
Guillermo Lescano <guillelescano.gl@gmail.com>
2023-11-08 18:46:22 -0300

    Enviando mail de Bienvenida en mailtrap

--- helpers/email.js
+++ helpers/email.js
@@ -1,7 +1,34 @@
 //vamos a colocar todos los emails, que van a ser dos, olvide, reset y el de confirmar
 
-import nodemailer from 'nodemailer'
+import nodemailer from "nodemailer";
 
-export const emailRegistro = (datos) =>{
-     console.log('Datos',datos)
-}
+export const emailRegistro = async(datos) => {
+  const { email, nombre, token } = datos;
+
+  //creamos esto que lo sacamos de mailtrap, con sis credenciales
+  const transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "60651dbe778ba4",
+      pass: "fef64292443c57",
+    },
+  });
+
+  //Informacion del email
+
+  const info = await transport.sendMail({ //sendmail envia el mail una vez configurado todo
+    from: '"UpTask - Administrador de proyecto" <cuentas@uptask.com>',
+    to: email,
+    subject: 'UpTask - Confirma tu cuenta',
+    text: "Comprueba tu cuenta en UpTask",
+    html: `<p>Hola: ${nombre} comprueba tu cuenta en UpTask</p>
+
+    <p>Tu cuenta ya esta casi lista, solo debes comprobarla en el siguiente enlace</p>
+    
+    <a href="${process.env.FRONTEND_URL}/confirmar-cuenta/${token}">Comprobar cuenta</a>
+
+    <p>Si tu no creaste esta cuenta puedes ignorar este email</p>
+    `
+  })
+};
